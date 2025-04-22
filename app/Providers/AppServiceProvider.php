286e77b958c6f1b7d670104a1fd310230c0015eb
Macbook Aswik <admin@MacBook-Pro-Aswik.local>
2025-04-22 22:26:11 +0800

    update verifikasi email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+        // return (new MailMessage)
+        //     ->subject('Verify Email Address')
+        //     ->line('Click the button below to verify your email address.')
+        //     ->action('Verify Email Address', $url);
+        return (new MailMessage)
+            ->subject('Verifikasi Email Kamu di BantuinDong')
+            ->greeting('Halo ' . $notifiable->name . ' 👋')
+            ->line('Terima kasih yah sudah menjadi bagian dari kami! Silakan klik tombol di bawah ini untuk memverifikasi email kamu.')
+            ->action('Verifikasi Email', $url)
+            ->line('Kalau kamu tidak merasa mendaftar, abaikan email ini ya. ')
+            ->salutation('Salam hangat, Tim BantuinDong');
+    });
     }
 }

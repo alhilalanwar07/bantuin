ac2ed8e94f497f0a8e559bc552a3285ae40f3cb1
Macbook Aswik <admin@MacBook-Pro-Aswik.local>
2025-05-05 22:11:19 +0800

    tambahkan foto customer pada list broadcast

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -764,7 +764,7 @@ class UserController extends Controller
             ->join('users', 'users.id', '=', 'customers.user_id')
             ->where('status_id', 1)
             ->select(
-                'service_requests.*','customers.name as customer_name','customers.profile_photo as customer_profile_photo',
+                'service_requests.*','customers.name as customer_name','users.profile_photo as customer_profile_photo',
                 DB::raw("6371 * acos(cos(radians($vendorLat)) * cos(radians(latitude)) * cos(radians(longitude) - radians($vendorLng)) + sin(radians($vendorLat)) * sin(radians(latitude))) AS distance")
             )
             ->whereIn('specialization_id', $specialization_id)

06b0e94c3812cd37e8f0dc53a78957c976a23d9a
Alhilal Anwar <alhilalanwar07@gmail.com>
2025-05-06 16:35:35 +0800

    update models costumer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Customer extends Model
 {
-    //
+    // Tentukan atribut yang dapat diisi
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'address',
+        'gender',
+    ];
+
+    /**
+     * Relasi dengan model User
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
